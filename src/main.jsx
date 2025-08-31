3167890c29871dbf9dbb4e4e004e12f26f51a1de
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-08-31 22:57:03 +0530

    routing added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,28 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './pages/Home.jsx';
+import BrowseEvents from './pages/BrowseEvents.jsx';
+import SubmitEvent from './pages/SubmitEvent.jsx';
+import Contact from './pages/Contact.jsx';
+import DetailedEventPage from './pages/DetailedEventPage.jsx';
 
 createRoot(document.getElementById('root')).render(
-  
+   <BrowserRouter>
+      <div className="bg-gray-900 text-gray-100 min-h-screen flex flex-col">
+        <Navbar />
+        <div className="container ">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/browse" element={<BrowseEvents />} />
+            <Route path="/submit" element={<SubmitEvent />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/events/:id" element={<DetailedEventPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
 )
