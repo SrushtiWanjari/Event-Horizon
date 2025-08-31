1945298c56c698f5504609018ad001cc9920bf14
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-08-31 23:50:41 +0530

    Footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
     <div>
-      
+    <footer className="bg-gray-950 text-gray-400 py-6 mt-8 ">
+      <div className="text-center">
+        <p>&copy; 2024 Event Horizon. All rights reserved.</p>
+        <div className="flex justify-center space-x-4 mt-2">
+          <a
+            href="#"
+            className="hover:text-white transition-colors duration-300"
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="#"
+            className="hover:text-white transition-colors duration-300"
+          >
+            Terms of Service
+          </a>
+        </div>
+      </div>
+    </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
