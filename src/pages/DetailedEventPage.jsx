4a085dd3b8787f7a41878b3ad1ada8534c7f3a5a
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-08-31 22:58:25 +0530

    basic code added

---
+++ src/pages/DetailedEventPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DetailedEventPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DetailedEventPage
---
+++ src/pages/SubmitEvent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SubmitEvent() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SubmitEvent
