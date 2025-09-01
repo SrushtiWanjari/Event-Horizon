27501c5bb596b9ae95cdc721c3f534b0a6b7b888
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-09-01 17:28:47 +0530

    featured card added

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { MapPin, Calendar } from "lucide-react";
+import { Link } from "react-router";
 
-function EventCard() {
+function EventCard({ event }) {
   return (
-    <div>
-      
+    <div className="bg-gray-800 rounded-lg  shadow-lg transform hover:scale-105 transition-transform duration-300 ">
+      <div className="p-5 w-105">
+        <h3 className="text-xl  font-semibold text-sky-400">{event.title}</h3>
+        <p className="text-gray-400 text-sm mt-1 flex items-center">
+          <Calendar size={14} className="mr-2" />
+          {event.date} | <MapPin size={14} className="ml-2 mr-1" />
+          {event.location}
+        </p>
+        <p className="text-md mt-3 pr-17">{event.description}</p>
+        <Link
+          to={`/events/${event.id}`}
+          className="mt-4 block w-50 text-center bg-teal-500 text-white font-bold py-2 rounded-full hover:bg-teal-600 transition-colors text-lg"
+        >
+          View Details
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default EventCard
+export default EventCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,20 +5,20 @@ import InitialEvents from "../data/InitialEvents.js"
 function Home() {
   return (
     <div>
-      <h1 className="text-5xl md:text-7xl font-extrabold  text-sky-500 animate-pulse text-center my-10">
+      <h1 className="text-5xl md:text-7xl font-extrabold  text-sky-500 animate-pulse text-center mt-50">
         Discover Your Next Adventure
       </h1>
 
-      <p className="mt-5 text-xl md:text-2xl text-gray-300 max-w-2xl mx-auto">
+      <p className="mt-10 text-xl md:text-2xl text-gray-300 max-w-2xl mx-auto mb-50">
         Find and explore local events happening right in your area.
       </p>
 
       <div className="mt-16">
-        <h2 className="text-3xl md:text-4xl font-bold text-teal-400 mb-6 border-b border-gray-700 pb-2">
+        <h2 className="text-3xl md:text-4xl font-bold text-teal-400 mb-6 border-b border-gray-700 pb-2 pl-10">
           Featured Events
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="flex flex-col justify-around sm:flex-col lg:flex-row gap-8">
           {InitialEvents.slice(0, 3).map((event) => (
             <EventCard key={event.id} event={event} />
           ))}
