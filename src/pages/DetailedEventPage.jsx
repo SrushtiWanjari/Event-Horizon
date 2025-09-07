6f583e68623235e8b7b032c6c2a85669ce175655
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-09-07 23:33:04 +0530

    code added

--- src/pages/DetailedEventPage.jsx
+++ src/pages/DetailedEventPage.jsx
@@ -23,6 +23,31 @@ function DetailedEventPage() {
       </div>
     );
   }
+
+  const musicSVG = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="h-4 w-4 mr-2 inline-block fill-current">
+      <path d="M492.6 195.4l-118-80c-7.9-5.4-19.3-5.4-27.2 0L229.4 195.4c-7.9 5.4-12.6 14.8-12.6 24.6V432c0 17.7-14.3 32-32 32h-16c-17.7 0-32-14.3-32-32s14.3-32 32-32h16c0-17.7 14.3-32 32-32s32 14.3 32 32v24.6l103.4-70.2c7.9-5.4 12.6-14.8 12.6-24.6V195.4zM32 160c-17.7 0-32 14.3-32 32s14.3 32 32 32h16c17.7 0 32-14.3 32-32s-14.3-32-32-32H32z"/>
+    </svg>
+  );
+
+  const foodSVG = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="h-4 w-4 mr-2 inline-block fill-current">
+      <path d="M224 96c-17.7 0-32-14.3-32-32s14.3-32 32-32h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H224zm-22.6 148.6c-4.2-4.2-11-4.2-15.2 0l-58.6 58.6c-4.2 4.2-4.2 11 0 15.2l20.6 20.6c4.2 4.2 11 4.2 15.2 0l58.6-58.6c4.2-4.2 4.2-11 0-15.2L201.4 244.6zM464 256c-13.2 0-24-10.8-24-24s10.8-24 24-24 24 10.8 24 24-10.8 24-24 24zm-144-48v-32c0-13.2-10.8-24-24-24h-32c-13.2 0-24 10.8-24 24v32c0 13.2 10.8 24 24 24h32c13.2 0 24-10.8 24-24zM256 0C114.6 0 0 114.6 0 256s114.6 256 256 256 256-114.6 256-256S397.4 0 256 0zm0 464c-114.9 0-208-93.1-208-208S141.1 48 256 48s208 93.1 208 208-93.1 208-208 208z"/>
+    </svg>
+  );
+
+  const getCategoryIcon = (category) => {
+    switch(category) {
+      case 'music':
+        return musicSVG;
+      case 'food':
+        return foodSVG;
+      case 'workshops':
+        return <Award size={14} className="mr-2" />;
+      default:
+        return null;
+    }
+  };
   return (
     <div>
       
