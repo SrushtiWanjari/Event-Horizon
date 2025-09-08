0b842b38475fa3f06a039b1162bca9fa7fa2472d
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-09-08 18:25:30 +0530

    create const using useState and useNavigate

--- src/pages/SubmitEvent.jsx
+++ src/pages/SubmitEvent.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import {useNavigate} from 'react-router';
 
 function SubmitEvent() {
+  const [formData, setFormData] = useState({ title: '', description: '', date: '', location: '', category: '' });
+  const navigate = useNavigate();
   return (
     <div>
       
