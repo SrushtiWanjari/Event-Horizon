1182a1b74b7d77237594d460b1187f91d84a886e
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-09-08 18:31:06 +0530

    event title input box added

--- src/pages/SubmitEvent.jsx
+++ src/pages/SubmitEvent.jsx
@@ -41,6 +41,13 @@ function SubmitEvent() {
   return(
   <div>
     <h2 className="text-3xl md:text-4xl font-bold text-teal-400 mb-6 border-b border-gray-700 pb-2">Submit Your Event</h2>
+      <div className="max-w-xl mx-auto bg-gray-800 rounded-lg p-8 shadow-lg"></div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label  className="block text-gray-300 font-bold mb-2">Event Title</label>
+            <input type="text"  name="title" value={formData.title} onChange={handleChange} className="w-full px-4 py-2 rounded-lg bg-gray-900 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-sky-500 transition-all" required />
+          </div>
+        </form>
   </div>
 );
 
