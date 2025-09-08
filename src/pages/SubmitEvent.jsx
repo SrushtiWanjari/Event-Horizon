956bca8bc1a987f2591abe6d617c6fbb5a043f46
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-09-08 18:26:12 +0530

    create handleChange function

--- src/pages/SubmitEvent.jsx
+++ src/pages/SubmitEvent.jsx
@@ -4,6 +4,11 @@ import {useNavigate} from 'react-router';
 function SubmitEvent() {
   const [formData, setFormData] = useState({ title: '', description: '', date: '', location: '', category: '' });
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+  };
   return (
     <div>
       
