c0b780e5f4db65695555076f6edfc2f9a9f990d9
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-09-20 22:02:14 +0530

    resolve path of InitialEvent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import EventCard from "../components/EventCard.jsx";
-import InitialEvents from "../data/InitialEvents.js"
+import InitialEvents from "../data/InitialEvents"
 
 function Home() {
   return (
